087114e3690fa8caf2e4be8e69203cb3fed0605b
nakategawa14 <tobiuotsukai@yahoo.co.jp>
2026-05-28 09:53:02 +0900

    Fix syntax error in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,12 @@
-{\rtf1\ansi\ansicpg932\cocoartf2869
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+import path from "path";
 
-\f0\fs24 \cf0 import path from "path";\
-\
-/** @type \{import('next').NextConfig\} */\
-const nextConfig = \{\
-  webpack: (config) => \{\
-    config.resolve.alias["@"] = path.resolve(__dirname);\
-    return config;\
-  \},\
-  reactStrictMode: true,\
-  output: "export",\
-\};\
-\
-export default nextConfig;\
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+  reactStrictMode: true,
+};
+
+export default nextConfig;
